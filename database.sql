576aca6843e33b8e4e2273e99a6d8085ac531d1b
ilya-ship-it <tulupov.2712@gmail.com>
2025-02-02 11:15:07 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMP NOT NULL
+);
